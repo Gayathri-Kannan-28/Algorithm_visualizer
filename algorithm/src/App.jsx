69d7c641da6cafa05a0b295201ee5d052a4460ce
Gayathri <gayathri.k2022cse@sece.ac.in>
2024-03-20 11:05:15 +0530

    Introduction

---
+++ algorithm/src/App.jsx
@@ -0,0 +1,11 @@
+import Backtheme from "./Components/Backtheme";
+
+const App=()=>{
+  return(
+    <>
+    <Backtheme/>
+    </>
+  )
+}
+
+export default App;
---
+++ algorithm/src/Components/Backtheme.jsx
@@ -0,0 +1,16 @@
+const Backtheme=()=>{
+    return(
+        <>
+        <section className="bg">
+           <div>
+            <img src="src\Components\backround.jpg" width="1000px" heigth="500px"></img>
+
+
+              
+           </div>
+        </section>
+        
+        </>
+    )
+}
+export default Backtheme;
