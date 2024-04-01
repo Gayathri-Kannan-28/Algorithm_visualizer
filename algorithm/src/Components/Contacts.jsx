844705ebeb0a47ed64c14d75535447bec0381279
Gayathri <gayathri.k2022cse@sece.ac.in>
2024-04-01 13:57:44 +0530

    Sidebar

--- algorithm/src/Components/Contacts.jsx
+++ algorithm/src/Components/Contacts.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 
 function Contacts() {
+
+
+    useEffect(() => {
+        getFeedDetails(); // Trigger expense details fetching when component mounts
+    }, []);
+
+    async function addnewEntry() {
+        const nameInput = document.getElementById('name');
+        const collegeInput = document.getElementById('college');
+        const viewInput = document.getElementById('view');
+
+        const name = nameInput.value;
+        const college = collegeInput.value;
+        const view = viewInput.value;
+
+        try {
+            const response = await fetch('https://loginapi-c4sh.onrender.com/add-feed', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name,
+                    college,
+                    view
+                })
+            });
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('Error adding entry:', error);
+        }
+    }
+
+    async function getFeedDetails() {
+        try {
+            const response = await fetch('https://loginapi-c4sh.onrender.com/get-feed');
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching expense details:', error);
+        }
+    }
+
+
   return (
      <>
      <section className="contact-content" id="contact">
@@ -15,8 +61,9 @@ function Contacts() {
                 <input type="email" name="EMAIL" placeholder="Email" required/>
                 
                 <input type="text" name="College" placeholder="College" id="college"/>
-                <textarea name="MESSAGE" placeholder="how its useful" id="view"></textarea>
-                <input type="submit" value="Send Feedback" className="submit"/>
+                <input name="MESSAGE" placeholder="how its useful" id="view"></input>
+        
+                <button  onClick={addnewEntry} value="Send Feedback" className="submit">Send Feedback</button>
                 
             </form>
            
